0f26a2a7613e37266890a798d8066daad4d49367
Boris Zdanko <bjdanko5@gmail.com>
2024-09-23 10:43:27 +0300

    без sudo

--- lsChangesServiceRun.bash
+++ lsChangesServiceRun.bash
@@ -1,12 +1,13 @@
-sudo systemctl  stop lsChanges
+systemctl  stop lsChanges
 cd /var/www/html/lsChanges
 go build -o lsChanges
 cp -f /var/www/html/lsChanges/lsChanges.service /etc/systemd/system/lsChanges.service
 sudo useradd -s /sbin/nologin -M lsChanges 
+mkdir -p /opt/lsChanges/
 cp  -f /var/www/html/lsChanges/*.html /opt/lsChanges/ 
 mv -f /var/www/html/lsChanges/lsChanges /opt/lsChanges/lsChanges
-sudo chmod 755 /opt/lsChanges/lsChanges
+chmod 755 /opt/lsChanges/lsChanges
 
-sudo systemctl daemon-reload
-sudo systemctl enable lsChanges
-sudo systemctl start lsChanges
+systemctl daemon-reload
+systemctl enable lsChanges
+systemctl start lsChanges
