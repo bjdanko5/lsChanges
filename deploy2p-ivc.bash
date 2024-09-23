e18ce182122b084239912bc3746f072ff66afdd8
Boris Zdanko <bjdanko5@gmail.com>
2024-09-23 10:39:21 +0300

    ssh

--- deploy2p-ivc.bash
+++ deploy2p-ivc.bash
@@ -1,13 +1,2 @@
-sudo systemctl  stop lsChanges
-cd /var/www/html/lsChanges
-go build -o lsChanges
-#/git push -f p-ivc-lsChanges main
-cp -f /var/www/html/lsChanges/lsChanges.service /etc/systemd/system/lsChanges.service
-sudo useradd -s /sbin/nologin -M lsChanges 
-cp  -f /var/www/html/lsChanges/*.html /opt/lsChanges/ 
-mv -f /var/www/html/lsChanges/lsChanges /opt/lsChanges/lsChanges
-sudo chmod 755 /opt/lsChanges/lsChanges
-
-sudo systemctl daemon-reload
-sudo systemctl enable lsChanges
-sudo systemctl start lsChanges
+git push -f p-ivc-lsChanges main
+ssh  ssh://user@192.168.10.130 /var/www/html/lsChanges/lsChangesServiceRun.bash
